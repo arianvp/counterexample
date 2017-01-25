84ec435e6bdc3a20563948ad022aa4ad09997f4a
Arian van Putten <aeroboy94@gmail.com>
2017-01-25 21:46:04 +0100

    counterexample

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+extern crate scoped_threadpool;
+fn main() {
+    let mut array = [0; 3];
+    let mut pool = scoped_threadpool::Pool::new(2);
+    pool.scoped(|scope| {
+        let array = &mut array;
+        scope.execute(move||{
+            array[0] = 1;
+        });
+        scope.execute(move||{
+            array[0] = 2;
+        });
+    });
+    println!("{:?}",array[0]);
+}
