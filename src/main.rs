32e9442a8c7a212caac716698b6b81e22fec65c1
Arian van Putten <aeroboy94@gmail.com>
2017-01-25 21:46:58 +0100

    counteR

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     let mut array = [0; 3];
     let mut pool = scoped_threadpool::Pool::new(2);
     pool.scoped(|scope| {
-        let array = &mut array;
+        //let array = &mut array;
         scope.execute(move||{
             array[0] = 1;
         });
